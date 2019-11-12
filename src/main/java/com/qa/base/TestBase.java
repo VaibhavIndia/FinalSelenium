47ba46cc8e4e152dd0c622d888f15959bcb819a9
vaibhav <vaibhav.nigade@gmail.com>
2019-11-12 19:01:40 +0530

    my First Commit

---
+++ src/main/java/com/qa/base/TestBase.java
@@ -0,0 +1,11 @@
+package com.qa.base;
+
+public class TestBase {
+
+	public static void main(String[] args) {
+		System.out.println("hi .. ");
+		System.out.println("code will start here");
+
+	}
+
+}
---
+++ src/test/java/com/qa/testcases/LoginTest.java
@@ -0,0 +1,10 @@
+package com.qa.testcases;
+
+public class LoginTest {
+	@Test
+	public void test1()
+	{
+		System.out.println("Test1");
+	}
+
+}
